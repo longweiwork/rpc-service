5541162ef04014bf37617e2515c2a9d2e31d6870
LongWei <longweiss521@gmail.com>
2020-08-19 13:55:38 +0800

    rpc框架

--- rpcservice/rpc-service-provider/src/main/java/client/ClientApplication.java
+++ rpcservice/rpc-service-provider/src/main/java/client/ClientApplication.java
@@ -1,5 +1,7 @@
 package client;
 
+import com.learn.IHelloService;
+
 /**
  * @Author: Brian
  * @Date: 2020/8/18 17:32
@@ -7,6 +9,8 @@ package client;
 public class ClientApplication {
 
     public static void main(String[] args) {
-
+        RpcProxyClient rpcProxyClient = new RpcProxyClient();
+        IHelloService iHelloService = rpcProxyClient.clientProxy(IHelloService.class, "localhost", 8080);
+        iHelloService.sayHello("hello");
     }
 }
